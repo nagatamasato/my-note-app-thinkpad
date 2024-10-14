aa46861f48e75eff6d8038e5fae55e0ccd604b77
gichico <hgtm.kabocha@gmail.com>
2024-10-14 11:41:59 +0900

    Styling the header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,10 +3,10 @@ import { View, Text, StyleSheet } from "react-native"
 const Index = (): JSX.Element => {
   return (
     <View style={styles.container}>
-      <View>
-        <View>
-          <Text>My Note App</Text>
-          <Text>Logout</Text>
+      <View style={styles.header}>
+        <View style={styles.headerInner}>
+          <Text style={styles.headerTitle}>My Note App</Text>
+          <Text style={styles.headerRight}>Logout</Text>
         </View>
       </View>
       <View>
@@ -52,8 +52,28 @@ const Index = (): JSX.Element => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    backgroundColor: '#ffffff'
+  },
+  header: {
+    backgroundColor: '#467FD3',
+    height: 104,
+    justifyContent: 'flex-end'
+  },
+  headerInner: {
     alignItems: 'center'
+  },
+  headerRight: {
+    position: 'absolute',
+    right: 16,
+    bottom: 16,
+    color: 'rgba(255, 255, 255, 0.7)'
+  },
+  headerTitle: {
+    marginBottom: 8,
+    fontSize: 22,
+    lineHeight: 32,
+    fontWeight: 'bold',
+    color: '#ffffff'
   }
 })
 
